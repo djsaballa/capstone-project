8e7a832a8d4cd237af851c255812fa269de9e9a9
jared's mod <71679362+JaredGalzote@users.noreply.github.com>
2023-04-23 21:32:33 +0800

    remove-edit

--- resources/views/pages/users/list-of-archive-users.blade.php
+++ resources/views/pages/users/list-of-archive-users.blade.php
@@ -122,10 +122,6 @@
                                         @endif
                                     </div>
                                     <div class="flex mt-4 lg:mt-0">
-
-                                        <a href="{{ route('edit_user', [$user_info->id, $user->id]) }}">
-                                            <button class="btn btn-secondary py-1 px-2 mr-2">Edit</button>
-                                        </a>
                                         <a href="{{ route('view_user', [$user_info->id, $user->id]) }}">
                                             <button class="btn btn-secondary py-1 px-2 mr-2">View</button>
                                         </a>
