35187f4c5c95dac87ac49076ce74541b16ba9279
djsaballa <danieljohnsaballa@student.laverdad.edu.ph>
2023-05-12 01:05:53 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,21 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\SecurityLevelSeeder;
+use Database\Seeders\RoleSeeder;
+use Database\Seeders\DivisionSeeder;
+use Database\Seeders\DistrictSeeder;
+use Database\Seeders\LocaleSeeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\DiseaseSeeder;
+use Database\Seeders\MedicalCategorySeeder;
+use Database\Seeders\ClientProfileSeeder;
+use Database\Seeders\FamilyCompositionSeeder;
+use Database\Seeders\MedicalConditionSeeder;
+use Database\Seeders\MedicalOperationSeeder;
+use Database\Seeders\ProgressReportSeeder;
+use Database\Seeders\InboxSeeder;
+use Database\Seeders\HistorySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +29,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            SecurityLevelSeeder::class,
+            RoleSeeder::class,
+            DivisionSeeder::class,
+            DistrictSeeder::class,
+            LocaleSeeder::class,
+            UserSeeder::class,
+            DiseaseSeeder::class,
+            MedicalCategorySeeder::class,
+            ClientProfileSeeder::class,
+            FamilyCompositionSeeder::class,
+            MedicalConditionSeeder::class,
+            MedicalOperationSeeder::class,
+            ProgressReportSeeder::class,
+            InboxSeeder::class,
+            HistorySeeder::class
+        ]);
     }
 }
