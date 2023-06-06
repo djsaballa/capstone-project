6902f55ae371ca46304f809a7bf08c898da09021
djsaballa <danieljohnsaballa@student.laverdad.edu.ph>
2023-06-06 21:08:57 +0800

    Update dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -328,15 +328,15 @@
                                 @endphp
                                 <select name="monthPicker" id="monthPicker" class="form-control sm:w-28 box pl-10">
                                     <option value="{{ $pickerMonth }}" selected>{{ date("M", mktime(0, 0, 0, $pickerMonth, 1)) }}</option>
-                                    <option value="1">Jan</option>
-                                    <option value="2">Feb</option>
-                                    <option value="3">Mar</option>
-                                    <option value="4">April</option>
-                                    <option value="5">May</option>
-                                    <option value="6">June</option>
-                                    <option value="7">July</option>
-                                    <option value="8">Aug</option>
-                                    <option value="9">Sept</option>
+                                    <option value="01">Jan</option>
+                                    <option value="02">Feb</option>
+                                    <option value="03">Mar</option>
+                                    <option value="04">April</option>
+                                    <option value="05">May</option>
+                                    <option value="06">June</option>
+                                    <option value="07">July</option>
+                                    <option value="08">Aug</option>
+                                    <option value="09">Sept</option>
                                     <option value="10">Oct</option>
                                     <option value="11">Nov</option>
                                     <option value="12">Dec</option>
